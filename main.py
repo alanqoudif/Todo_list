dfe168987123316f26d9dc9968f14ccdddc39203
faisal al anqoudi <54464056+oman619@users.noreply.github.com>
2023-10-10 23:45:28 +0400

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# استيراد الأشياء المهمة
+# استيراد من المكتبات 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
@@ -6,42 +6,42 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# جعل الأشياء تعمل مع بعضها البعض
+
 class TodoItem(BaseModel):
     title: str
-    description: str = "No description"  # الأشياء لازم تكون واضحة
+    description: str = "No description"  
     done: bool = False
 
 all_of_my_todos = []  # هنا نحفظ الأشياء
 
-origins = ["http://localhost:8000", "http://127.0.0.1:8000"]  # هذه الأماكن مهمة
+origins = ["http://localhost:8000", "http://127.0.0.1:8000"]  # هذه الروابط التابعه للمشروع 
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,  # هذا مهم جدا
-    allow_methods=["*"],  # لا أعلم ماذا يعني ولكن يجب أن يكون مهمًا
+    allow_methods=["*"],
     allow_headers=["*"],  # نفس الشيء هنا
 )
 
-@app.post("/todos/")  # يجب أن يكون هناك مكان لوضع الأشياء الجديدة
+@app.post("/todos/")  #  مكان لوضع الأشياء الجديد
 def create_a_todo(todo: TodoItem):
-    all_of_my_todos.append(todo)  # الأشياء تذهب هنا
-    return todo  # هل يجب أن أعيده؟
+    all_of_my_todos.append(todo)  # الأشياء تروح هنا
+    return todo  
 
-@app.get("/todos/")  # أريد رؤية الأشياء
+@app.get("/todos/") 
 def get_all_todos():
     return all_of_my_todos
 
-@app.get("/todos/{id_of_todo}")  # أريد رؤية شيء واحد
+@app.get("/todos/{id_of_todo}")
 def get_a_single_todo(id_of_todo: int):
-    if id_of_todo >= len(all_of_my_todos):  # هذا يبدو صحيحاً؟
+    if id_of_todo >= len(all_of_my_todos):  
         return {"error": "not found!! "}
     return all_of_my_todos[id_of_todo]
 
-@app.put("/todos/{id_of_todo}")  # أريد تغيير شيء ما
+@app.put("/todos/{id_of_todo}")  
 def change_a_todo(id_of_todo: int, todo: TodoItem):
-    if id_of_todo >= len(all_of_my_todos):  # هذا يجب أن يكون نفس الشيء
+    if id_of_todo >= len(all_of_my_todos):  
         return {"error": "not found!! "}
-    all_of_my_todos[id_of_todo] = todo  # الشيء الجديد يحل محل القديم
-    return todo  # أعتقد أنه يجب إرجاعه
+    all_of_my_todos[id_of_todo] = todo  
+    return todo 
